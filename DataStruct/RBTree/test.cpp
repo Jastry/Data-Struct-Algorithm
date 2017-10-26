a477370c5655fcbacbe56b36d2ae38523b8ea297
Jastry <blbagony@163.com>
2017-10-26 23:20:49 +0800

    插入错误

--- DataStruct/RBTree/test.cpp
+++ DataStruct/RBTree/test.cpp
@@ -2,22 +2,25 @@
 #include <time.h>
 #include <stdlib.h>
 
-typedef RBTreeNode<int, int> Node;
 
 
-int main(int argc, char** argv)
+int main()
 {
 
 	RBTree<int, int> t;
 	srand((unsigned)time(NULL));
 	for (int i = 0; i < 100; i++){
 		int n = rand() % 50000;
-		t.insert(i,i);
+		t.Insert(n);
 	}
-
 	
-	t.prevOder();
-	t.postOder();
-	std::cout << t.is_rb_tree() << std::endl;
+	RBTree<int,int>::Iterator it = t.Begin();
+	while (it != t.End()){
+		std::cout << *it <<" "; 
+	}
+	std::cout << std::endl;
+	//t.inOder();
+	//t.postOder();
+	//std::cout << t.is_rb_tree() << std::endl;
 	return 0;
 }
