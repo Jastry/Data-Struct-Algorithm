b6d03d4f54a83bc650fe0a31febb082c2995a892
Jastry <blbagony@163.com>
2017-11-19 19:09:43 +0800

    add quick sort

--- Sort/test.cpp
+++ Sort/test.cpp
@@ -2,23 +2,20 @@
 
 int main()
 {
-    int a[5000];
-    srand((unsigned)time(NULL));
-    for (int i = 0; i < 5000; ++i) {
-        a[i] = rand() % 13131;
-    }
-    InsertSort2(a, sizeof (a) / sizeof (int));
+    int a[] = {9, 0, 6, 7, 3, 1, 8, 4, 2, 5};
+    //int a[5000];
+    //srand((unsigned)time(NULL));
+    //for (int i = 0; i < 5000; ++i) {
+    //    a[i] = rand() % 13131;
+    //}
+    //InsertSort2(a, sizeof (a) / sizeof (int));
     //ShellSort(a, sizeof (a) / sizeof (int));
     //SelectSort(a, sizeof (a) / sizeof (int));
-//    for (size_t i = 0; i < 100; ++i) {
-//        std::cout << a[i] << " ";
-//    }
-//    std::cout << std::endl;
     //HeapSort(a, sizeof (a) / sizeof (int)) ;   
+    //BubbleSort(a, sizeof (a) / sizeof (int)) ;   
+    //QuickSortNonR(a, 0, sizeof (a) / sizeof (int) - 1);
+    //QuickSort(a, 0, sizeof (a) / sizeof (int) - 1)
+    CountSort(a, sizeof (a) / sizeof (int));
     IsSorted(a, sizeof (a) / sizeof (int));
-    for (int i = 0 ; i < 1000; ++i) {
-        std::cout << a[i] << " ";
-    }
-    std::cout << std::endl;
     return 0;
 }
